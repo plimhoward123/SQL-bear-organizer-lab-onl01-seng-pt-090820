b46b80f2c6a5dc3f6c02152bc36e7401ee44ff2a
Plimon Howard <plimon.howard@gmail.com>
2020-11-20 20:55:43 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -4,6 +4,7 @@ end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
   rtrn = "SELECT name FROM bears;"
+  puts rtrn
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
